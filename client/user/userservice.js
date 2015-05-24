8343e8604a9e91aac97065664460b2bde44224be
Seth Anderson <seth.anderson32@gmail.com>
2015-05-24 00:28:33 -0500

    Added user service

---
+++ client/user/userservice.js
@@ -0,0 +1,77 @@
+/*
+-----------------------------------------------------------------------------
+Copyright (c) 2014-2015 Seth Anderson
+
+This software is provided 'as-is', without any express or implied warranty. 
+In no event will the authors be held liable for any damages arising from the 
+use of this software.
+
+Permission is granted to anyone to use this software for any purpose, 
+including commercial applications, and to alter it and redistribute it 
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not 
+claim that you wrote the original software. If you use this software in a 
+product, an acknowledgment in the product documentation would be appreciated 
+but is not required.
+
+2. Altered source versions must be plainly marked as such, and must not be 
+misrepresented as being the original software.
+
+3. This notice may not be removed or altered from any source distribution.
+-----------------------------------------------------------------------------
+*/
+
+(function(){
+	var UserService = function($http, $q){
+		return {
+			register: function(postData){
+				var deferred = $q.defer();
+				$http.post("/api/user/register", postData)
+				.then(
+					function(response){
+						deferred.resolve(response.data);
+					},
+					function(response){
+						deferred.reject("Unable to register user");
+					}
+				);
+				return deferred.promise;
+			},
+
+			login: function(email, pass){
+				var deferred = $q.defer();
+				$http.post("/api/user/login", {email: email, password: pass})
+				.then(
+					function(response){
+						deferred.resolve();
+					},
+					function(response){
+						deferred.reject("Invalid username or password");
+					}
+				);
+				return deferred.promise;
+			},
+
+			logout: function(){
+				var deferred = $q.defer();
+				$http.post("/api/user/logout")
+				.then(
+					function(response){
+						deferred.resolve();
+					},
+					function(response){
+						deferred.reject();
+					}
+				);
+				return deferred.promise;
+			}
+		};
+	};
+
+	angular
+		.module("user", [])
+		.factory("UserService", UserService);
+
+	UserService.$inject = ["$http", "$q"];
+})();
