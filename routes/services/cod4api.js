8da59b499bb3ab5e6145bf75a454cc4d4a930536
Seth Anderson <seth.anderson32@gmail.com>
2015-08-07 23:03:59 -0500

    Fixed error introduced in cod4 api

--- routes/services/cod4api.js
+++ routes/services/cod4api.js
@@ -26,6 +26,7 @@ var cod4 = require("../../utils/cod4-rcon.js"),
 	authorize = require("../../authorization.js").authorize,
 	authenticate = require("../../utils/common.js").authenticate,
 	loadConfig = require("../../utils/common.js").loadConfig,
+	log = require("../../utils/log.js"),
 	gameinfo = loadConfig(__dirname + "/../../config/cod4-gameinfo.json");
 
 module.exports = function(app, prefix){
